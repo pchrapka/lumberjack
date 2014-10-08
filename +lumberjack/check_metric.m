3ca24872a0d908d194683589c9a5ef4d0b608425
pchrapka <pchrapka@gmail.com>
2014-10-08 12:14:03 -0400

    setting up functions to access metric data

---
+++ +lumberjack/check_metric.m
@@ -0,0 +1,55 @@
+function [out] = check_metric(cfg, metric)
+%CHECK_METRIC checks the metric for a match to cfg
+%
+%   cfg specifies query parameters for the metric of interest
+%   cfg.name
+%       (string, required), name of the metric
+%   cfg.(metric-specific-fields)
+%       (optional) arbitrary field specifying additional information about
+%       the metric, this is optional and dependent on the desired metric
+%
+%   metric has the following structure
+%       metric.name 
+%           (required)
+%       metric.output 
+%           (required)
+%       metric.(metric-specific-field) 
+%           (optional), these fields contain extra information pertaining
+%           to the metric, typically configuration parameters
+
+% Check the metric name
+pattern =  regexptranslate('escape', cfg.name);
+out = regexp(metric.name, ['^' pattern '$']);
+
+if isempty(out)
+    out = 0;
+    return;
+end
+
+% Check other metric specific fields
+names = fieldnames(cfg);
+for i=1:length(names)
+    field = names{i};
+    field_val = cfg.(field);
+    
+    % Check the field, based on the type of the value in the field
+    if ischar(field_val)
+        pattern =  regexptranslate('escape', field_val);
+        out = regexp(metric.(field), ['^' pattern '$']);
+    elseif isinteger(field_val)
+        out = metric.(field) == field_val;
+    else
+        disp(cfg.(field));
+        error('lumberjack:check_metric',...
+            'unknown value type');
+    end
+    
+    % Check the result
+    if isempty(out)
+        out = 0;
+        return;
+    end
+    
+end
+
+end
